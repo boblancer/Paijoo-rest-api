99bbebcffb2317aff9a322d6ba389aa3e3fcb87a
Bob <janekarnkit2013@hotmail.com>
2019-11-19 00:11:08 +0700

    rewrite unfinished UserController

--- src/main/java/com/example/paijoo_rest_api/controllers/UsersController.java
+++ src/main/java/com/example/paijoo_rest_api/controllers/UsersController.java
@@ -3,10 +3,13 @@ package com.example.paijoo_rest_api.controllers;
 import com.example.paijoo_rest_api.model.RequestBody.CreateUserRequest;
 import com.example.paijoo_rest_api.model.RequestBody.SendMessage;
 import com.example.paijoo_rest_api.model.RequestBody.TextContentRequestBody;
+import com.example.paijoo_rest_api.model.database.Users;
 import com.example.paijoo_rest_api.services.MessagesService;
 import com.example.paijoo_rest_api.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 
 @CrossOrigin
@@ -22,9 +25,15 @@ public class UsersController {
     }
 
     @PostMapping("/create")
-    CreateUserRequest send(@RequestBody CreateUserRequest req){
+    CreateUserRequest createUser(@RequestBody CreateUserRequest req){
         usersService.registerUser(req.getUsername(), req.getPassword());
         return req;
+
+    }
+
+    @GetMapping("/{id}/friends")
+    ArrayList<Users> getFriendlist(@PathVariable("id") int id ){
+        return usersService.getFriendListById(id);
 
     }
 }
