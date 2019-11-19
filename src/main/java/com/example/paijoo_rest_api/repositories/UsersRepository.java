91dbcada22b2c0b3e10500ed5114cea1f26f372a
Bob <janekarnkit2013@hotmail.com>
2019-11-19 18:28:30 +0700

    change query

--- src/main/java/com/example/paijoo_rest_api/repositories/UsersRepository.java
+++ src/main/java/com/example/paijoo_rest_api/repositories/UsersRepository.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
-    @Query(value="SELECT * from users u " +
+    @Query(value="SELECT * from " +
             "friendship f RIGHT JOIN users u ON f.user_id = u.id " +
             "where f.user_id = :id"
             , nativeQuery = true)
